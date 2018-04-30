2315e2acfe796581b6568d55c735dc5a2fdcc420
Liad Magen <liad.magen@gmail.com>
2018-04-30 12:40:15 +0200

    moved connection_string to os.environ

--- databaseconfig.py
+++ databaseconfig.py
@@ -1,1 +1,4 @@
-mongo_db = { 'connection_string': 'your_mongo_connection_string' }
+import os
+
+# mongo_db = { 'connection_string': 'your_mongo_connection_string' }
+mongo_db = { 'connection_string': os.environ['mongo_connection_string']}
