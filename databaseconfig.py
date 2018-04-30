285b77d720e277adb6cdc1025c4210b6aa58dccc
Liad Magen <liad.magen@gmail.com>
2018-04-30 14:32:25 +0200

    uppercased the setting

--- databaseconfig.py
+++ databaseconfig.py
@@ -1,4 +1,4 @@
 import os
 
 # mongo_db = { 'connection_string': 'your_mongo_connection_string' }
-mongo_db = { 'connection_string': os.environ['mongo_connection_string']}
+mongo_db = { 'connection_string': os.environ['MONGO_CONNECTION_STRING']}
